bcf55be3569710a2ec7b7b7106588a133a98601b
Luthfan Ali <mail@luthfanali.com>
2024-07-11 12:14:31 +0700

    add: insert jupyter to main app

--- src/LazadaIDReviews/entity/config_entity.py
+++ src/LazadaIDReviews/entity/config_entity.py
@@ -1,38 +1,50 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-"""NOTE: Delete or replace any class as you need
-and don't forget to import this class in
-'../config/configuration.py' or 
-'src/LazadaIDReviews/config/configuration.py'
-"""
+# NOTE: 'frozen=True' in dataclass decorator
+# means you can't add any new functionality
 
 @dataclass(frozen=True)
-class DataIngestionConfig:
+class DataIngestionSQLConfig:
     root_dir: Path
-    input_train_path: Path
-    input_test_path: Path
-    
+    source_URI: str
+    reviews_table: str
+    reviews_path: Path
+    items_table: str
+    items_path: Path
+    category_table: str
+    category_path: Path
+
 @dataclass(frozen=True)
-class PreprocessingConfig:
+class DataDumpConfig:
     root_dir: Path
+    reviews_path: Path
     input_train_path: Path
     input_test_path: Path
     output_train_path: Path
     output_test_path: Path
+    params_test_size: float
+
+@dataclass(frozen=True)
+class DataPreprocessingConfig:
+    root_dir: Path
+    input_train_path: Path
+    input_test_path: Path
+    vectorized_train_path: Path
+    vectorized_test_path: Path
+    model_dir: Path
+    vectorizer_model_path: Path
 
 @dataclass(frozen=True)
 class TrainingConfig:
     root_dir: Path
     input_train_path: Path
-    input_test_path: Path
     output_train_path: Path
-    output_test_path: Path
+    vectorized_train_path: Path
     model_path: Path
-    params_batch_size: int
-    params_epoch: int
-    params_classes: int
-    params_lr: float
+    params_max_iter: int
+    params_solver: str
+    params_n_jobs: int
 
 @dataclass(frozen=True)
 class TrainEvaluationConfig:
@@ -41,9 +53,17 @@ class TrainEvaluationConfig:
     input_test_path: Path
     output_train_path: Path
     output_test_path: Path
+    vectorized_train_path: Path
+    vectorized_test_path: Path
+    vectorizer_model_path: Path
     model_path: Path
     score_path: Path
     mlflow_dataset_path: Path
+    mlflow_dataset_column: list
+    minio_endpoint_url: str
+    minio_access_key_id: str
+    minio_secret_access_key: str
     mlflow_tracking_uri: str
     mlflow_exp_name: str
+    mlflow_dataset_bucket: str
     mlflow_run_name: str
